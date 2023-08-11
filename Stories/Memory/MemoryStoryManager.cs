e2074c18ece74541a74fce167f54b6fa9635a70f
Russell Froehlich <BigRedProf@outlook.com>
2023-08-11 10:36:41 -0700

    fixed concurrency bug in MemoryStoryManager

--- Stories/Memory/MemoryStoryManager.cs
+++ Stories/Memory/MemoryStoryManager.cs
@@ -1,5 +1,4 @@
-﻿using BigRedProf.Data;
-using BigRedProf.Stories.Models;
+﻿using BigRedProf.Stories.Models;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -9,20 +8,22 @@ namespace BigRedProf.Stories.Memory
 	public class MemoryStoryManager
 	{
 		#region fields
-		private IDictionary<StoryId, ObservableCollection<StoryThing>> _dictionary;
+		private IDictionary<StoryId, ObservableCollection<StoryThing>> _storyThingsDictionary;
+		private IDictionary<StoryId, MemoryScribe> _scribeDictionary;
 		#endregion
 
 		#region constructors
 		public MemoryStoryManager()
 		{
-			_dictionary = new ConcurrentDictionary<StoryId, ObservableCollection<StoryThing>>();
+			_storyThingsDictionary = new ConcurrentDictionary<StoryId, ObservableCollection<StoryThing>>();
+			_scribeDictionary = new ConcurrentDictionary<StoryId, MemoryScribe>();
 		}
 		#endregion
 
 		#region methods
 		public MemoryScribe GetScribe(StoryId id)
 		{
-			return new MemoryScribe(GetOrCreateListOfThings(id));
+			return GetOrCreateScribe(id);
 		}
 
 		public MemoryStoryteller GetStoryteller(StoryId id)
@@ -40,13 +41,25 @@ namespace BigRedProf.Stories.Memory
 		private ObservableCollection<StoryThing> GetOrCreateListOfThings(StoryId id)
 		{
 			ObservableCollection<StoryThing>? things = null;
-			if(!_dictionary.TryGetValue(id, out things))
+			if(!_storyThingsDictionary.TryGetValue(id, out things))
 			{
 				things = new ObservableCollection<StoryThing>();
-				_dictionary.Add(id, things);
+				_storyThingsDictionary.Add(id, things);
 			}
 
 			return things;
+		}
+
+		private MemoryScribe GetOrCreateScribe(StoryId id)
+		{
+			MemoryScribe? scribe = null;
+			if(!_scribeDictionary.TryGetValue(id, out scribe))
+			{
+				scribe = new MemoryScribe(GetOrCreateListOfThings(id));
+				_scribeDictionary.Add(id, scribe);
+			}
+
+			return scribe;
 		}
 		#endregion
 	}
