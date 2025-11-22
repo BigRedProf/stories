54d2fd175f660fdc4828d6cad27ba3b763840fa1
Russell Froehlich <BigRedProf@outlook.com>
2025-11-21 16:28:35 -0800

    added a StoriesCLI unit test project

---
+++ src/StoriesCli.Test/BackupCommandTests.cs
@@ -0,0 +1,13 @@
+namespace BigRedProf.Stories.StoriesCli.Test;
+
+public class BackupCommandTests
+{
+	#region unit tests
+	[Trait("Region", "BackupCommand methods")]
+	[Fact]
+    public void Test1()
+    {
+		Assert.True(true);
+	}
+	#endregion
+}
